f0e2132421e4c627e48af26f7308585876cb8683
Alesia <alesia.kurlenia@gmail.com>
2019-06-07 14:57:39 +0300

    Contacts

---
+++ Contacts/Contacts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Contacts
+//
+//  Created by Alesia Adereyko on 07/06/2019.
+//  Copyright © 2019 Alesia Adereyko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Contacts/Contacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Contacts
+//
+//  Created by Alesia Adereyko on 07/06/2019.
+//  Copyright © 2019 Alesia Adereyko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
